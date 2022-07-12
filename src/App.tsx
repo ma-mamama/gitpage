ea0a57a1e1254e90a39272a4e8b39b8621c22ca9
ma-mamama <mamamamam.2525@gmail.com>
2022-07-13 02:15:23 +0900

    BouserRouterとRouterモジュール追加＆骨組み構築

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './pages/Header'
+import Fotter from './pages/Fotter';
+import Top from './pages/Top';
+
+import {BrowserRouter,Routes , Route} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <BrowserRouter>
+       <Routes>
+        <Route path="gitpage" >
+         <Route path='top' element={<Top/>} />
+         <Route path=''/>
+        </Route>
+       </Routes>
+      </BrowserRouter>
+      <Fotter />
+    </>
   );
 }
 
---
+++ src/pages/Top.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const top = () => {
+    return (
+       <div>contents</div> 
+    )
+}
+
+export default top
